bd50b794c419456f314cca1c2c8f6d64c974d45e
nhuancaq <natalia.lizethqq@gmail.com>
2026-07-09 20:56:08 -0600

    Agregar el menú principal

---
+++ proyecto/main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <string>
+
+#include "denuncia.h"
+#include "seguridad.h"
+
+using namespace std;
+
+// Función que muestra el menú principal
+
+void menu(){
+
+    cout << "=========================================================\n";
+    cout << "      SISTEMA DE REPORTES DE INSEGURIDAD CIUDADANA\n";
+    cout << "                    TACNA - PERU\n";
+    cout << "=========================================================\n\n";
+
+    cout << "   [1] Registrar incidente\n\n";
+
+    cout << "   [2] Mostrar incidentes\n\n";
+
+    cout << "   [3] Buscar incidente\n\n";
+
+    cout << "   [4] Modificar incidente\n\n";
+
+    cout << "   [5] Eliminar incidente\n\n";
+
+    cout << "   [6] Consejos de seguridad\n\n";
+
+    cout << "   [0] Salir\n\n";
+
+    cout << "=========================================================\n";
+
+    cout << "Seleccione una opcion: ";
+
+}
+
+// Programa principal
+
+int main(){
+
+    int opcion;
+
+    do{
+
+        system("cls");
+
+        menu();
+
+        cin >> opcion;
+
+        cin.ignore();
+
+        system("cls");
+
+        switch(opcion){
+
+            case 1:
+
+                registrarIncidente();
+
+                break;
+
+            case 2:
+
+                mostrarIncidentes();
+
+                break;
+
+            case 3:
+
+                buscarIncidente();
+
+                break;
+
+            case 4:
+
+                modificarIncidente();
+
+                break;
+
+            case 5:
+
+                eliminarIncidente();
+
+                break;
+
+            case 6:
+
+                consejosSeguridad();
+
+                break;
+
+            case 0:
+
+                cout << "\nGracias por utilizar el sistema de reportes \n";
+
+                break;
+
+            default:
+
+                cout << "\nOpcion invalida \n";
+
+        }
+
+        if(opcion!=0){
+
+            cout << "  Presione ENTER para continuar...";
+
+            cin.get();
+
+        }
+
+    }while(opcion!=0);
+
+    return 0;
+
+}

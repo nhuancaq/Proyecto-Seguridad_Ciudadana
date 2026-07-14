09fb2e8e5efb59e1711e75fc01a3edaa4a0c77f5
Jose Nina <jninaa@unjbg.edu.pe>
2026-07-13 19:34:32 -0500

    Añadir Seguridad.cpp

---
+++ Seguridad.cpp
@@ -0,0 +1,17 @@
+#include "seguridad.h"
+
+// Implementación de la Opción 7: Recomendaciones de seguridad
+void consejosSeguridad() {
+    cout << "\n===========================================" << endl;
+    cout << "        RECOMENDACIONES DE SEGURIDAD       " << endl;
+    cout << "===========================================" << endl;
+    cout << "1. " << "Evite caminar por calles poco iluminadas." << endl;
+    cout << "2. " << "No exhiba objetos de valor." << endl;
+    cout << "3. " << "Llame a emergencias si observa un delito." << endl;
+    cout << "4. " << "Mantenga contacto con familiares al trasladarse." << endl; 
+    cout << "===========================================" << endl;
+    cout << "Presione ENTER para regresar al menu...";
+    cin.ignore();
+    cin.get();
+}
+

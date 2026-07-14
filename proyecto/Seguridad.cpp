050a540eabb4465c24a2a6f0c2dc69258b1d6165
Jose Nina <jninaa@unjbg.edu.pe>
2026-07-13 20:19:09 -0500

    Añadir Seguridad.cpp

--- proyecto/Seguridad.cpp
+++ proyecto/Seguridad.cpp
@@ -1,5 +1,13 @@
 #include "seguridad.h"
+using namespace std;
 
 void consejosSeguridad(){
-
+	cout << "\n===========================================" << endl;
+	cout << "         RECOMENDACIONES DE SEGURIDAD       " << endl;
+	cout << "===========================================" << endl;
+	cout << "[-] 1. Evite caminar por calles poco iluminadas." << endl; //
+	cout << "[-] 2. No exhiba objetos de valor." << endl; //
+	cout << "[-] 3. Llame a emergencias si observa un delito." << endl; //
+	cout << "[-] 4. Mantenga contacto con familiares al trasladarse." << endl; //
+	cout << "===========================================\n" << endl;
 }

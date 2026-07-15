c588d7e44cf8519743ba9d75651b26ac73591031
Jose Nina <jninaa@unjbg.edu.pe>
2026-07-14 21:07:08 -0500

    Agrego numeros de emergencia

--- proyecto/Seguridad.cpp
+++ proyecto/Seguridad.cpp
@@ -1,14 +1,18 @@
 #include "seguridad.h"
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void consejosSeguridad(){
-	cout << "\n===========================================" << endl;
-	cout << "         RECOMENDACIONES DE SEGURIDAD       " << endl;
-	cout << "===========================================" << endl;
-	cout << "[-] 1. Evite caminar por calles poco iluminadas." << endl; //
-	cout << "[-] 2. No exhiba objetos de valor." << endl; //
-	cout << "[-] 3. Llame a emergencias si observa un delito." << endl; //
-	cout << "[-] 4. Mantenga contacto con familiares al trasladarse." << endl; //
-	cout << "===========================================\n" << endl;
+    cout << "\n=======================================================" << endl;
+    cout << "          RECOMENDACIONES DE SEGURIDAD EN TACNA        " << endl;
+    cout << "=======================================================" << endl;
+    cout << "[-] 1. Evite caminar por calles poco iluminadas." << endl; 
+    cout << "[-] 2. No exhiba objetos de valor." << endl; 
+    cout << "[-] 3. Mantenga contacto con familiares al trasladarse." << endl; 
+    cout << "[-] 4. DIRECTORIO DE EMERGENCIAS:" << endl;
+    cout << "       * Policia Nacional (Central Tacna): 105 o (052)414141 " << endl;
+    cout << "       * Serenazgo / Seguridad Ciudadana: (052)314028" << endl;
+    cout << "       * Bomberos: 116 o (052)425833 " << endl;
+    cout << "       * SAMU (Ambulancias): 106 o (052)241994" << endl;
+    cout << "=======================================================\n" << endl;
 }
